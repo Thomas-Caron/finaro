dd6b188f90f97fb87712afbfe762a1a7c5799858
Thomas Caron <thomas.caron.dev@gmail.com>
2025-04-15 14:27:49 +0200

    feat: Create Tooltip component

---
+++ assets/vue/components/tootlip/Tooltip.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :id="id" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white dark:text-stone-950 transition-opacity duration-300 bg-stone-950 rounded-lg shadow-xs opacity-0 tooltip dark:bg-stone-50">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    id: {
+        type: String,
+        required: true
+    }
+});
+</script>
